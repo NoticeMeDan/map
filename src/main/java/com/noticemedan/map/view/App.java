f7f005470659b8ce1a6e387afb00dd18f35f2edc
Elias Jørgensen <eliaszakarias@gmail.com>
2018-03-15 14:11:05 +0100

    Revert "Draw some lines"
    This reverts commit dff614c8b9af0823d3ecf6e48701347fceaf776f.

--- src/main/java/com/noticemedan/map/view/App.java
+++ src/main/java/com/noticemedan/map/view/App.java
@@ -1,21 +1,7 @@
 package com.noticemedan.map.view;
 
-import com.noticemedan.map.viewmodel.PaneView;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        PaneView paneView = new PaneView();
-        primaryStage.setTitle("SimpleMap");
-        primaryStage.setScene(new Scene(paneView.getRoot()));
-        primaryStage.sizeToScene();
-        primaryStage.show();
-    }
-
+public class App {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("Hi mom!");
     }
 }
