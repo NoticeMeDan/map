1d5a0d2b9416b1cf832b4b01039e811602ca89ae
IMarnild <im.arnild@gmail.com>
2018-03-15 14:25:51 +0100

    works now

--- src/main/java/com/noticemedan/map/viewmodel/MapModel.java
+++ src/main/java/com/noticemedan/map/viewmodel/MapModel.java
@@ -6,6 +6,9 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,45 +16,39 @@ import java.util.List;
 public class MapModel implements Iterable<Path> {
 
     private List<Path> paths;
-    private BufferedFileReader bufferedFileReader;
-    private String fileName;
+    private String filename;
 
-    public MapModel(String fileName) {
+    public MapModel(String filename) {
         this.paths = new ArrayList<>();
-        this.bufferedFileReader = new BufferedFileReader();
-        this.fileName = fileName;
+        this.filename = filename;
         fillPathList();
     }
 
     private void fillPathList() {
-        BufferedReader bf = bufferedFileReader.createBufferedReader(fileName);
         try {
-            for (String line = bf.readLine(); line != null; bf.readLine()) {
+            BufferedReader b = new BufferedReader(new FileReader("src/" + filename));
+            for (String line = b.readLine(); line != null; line = b.readLine() ) {
                 String[] tokens = line.split(" ");
                 if (tokens[0].equals("LINE")) {
-                    double[] coordinates = new double[4];
-                    for (int i = 0; i < 4; i++) {
-                        coordinates[i] = Double.parseDouble(tokens[i+1]);
-                    }
-                    paths.add(createLinePath(coordinates));
+                    double x1 = Double.parseDouble(tokens[1]);
+                    double y1 = Double.parseDouble(tokens[2]);
+                    double x2 = Double.parseDouble(tokens[3]);
+                    double y2 = Double.parseDouble(tokens[4]);
+                    Path path = new Path();
+                    MoveTo moveTo = new MoveTo();
+                    LineTo lineTo = new LineTo();
+                    moveTo.setX(x1); moveTo.setY(y1);
+                    lineTo.setX(x2); lineTo.setY(y2);
+                    path.getElements().add(moveTo);
+                    path.getElements().add(lineTo);
+                    paths.add(path);
                 }
             }
-        } catch (Exception ioEx) {
-            ioEx.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-
-    private Path createLinePath(double[] coordinates) {
-        Path path = new Path();
-        MoveTo moveTo = new MoveTo();
-        LineTo lineTo = new LineTo();
-
-        moveTo.setX(coordinates[0]); moveTo.setY(coordinates[1]);
-        lineTo.setX(coordinates[2]); lineTo.setY(coordinates[3]);
-        path.getElements().add(moveTo);
-        path.getElements().add(lineTo);
-
-        return path;
     }
 
     @Override
