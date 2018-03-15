dff614c8b9af0823d3ecf6e48701347fceaf776f
SHOE <shoe@itu.dk>
2018-03-15 13:32:53 +0100

    Draw some lines

--- src/main/java/com/noticemedan/map/view/App.java
+++ src/main/java/com/noticemedan/map/view/App.java
@@ -1,7 +1,21 @@
 package com.noticemedan.map.view;
 
-public class App {
+import com.noticemedan.map.viewmodel.PaneView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        PaneView paneView = new PaneView();
+        primaryStage.setTitle("SimpleMap");
+        primaryStage.setScene(new Scene(paneView.getRoot()));
+        primaryStage.sizeToScene();
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hi mom!");
+        launch(args);
     }
 }
