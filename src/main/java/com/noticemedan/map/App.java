d6bfaa158ba6f60878f970197f5a0d9ef179eb65
Elias Jørgensen <eliaszakarias@gmail.com>
2018-03-21 21:48:05 +0100

    Always initialize lists in Osm

--- src/main/java/com/noticemedan/map/App.java
+++ src/main/java/com/noticemedan/map/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
 		XMLMapData xmlMapper = new XMLMapData();
 		File file = new File(String.valueOf(App.class.getResource("/smaller.osm")).split(":")[1]);
-		//File file = new File("/home/elias/Projects/Code/map/denmark-latest.os");
+		//File file = new File("/home/elias/Projects/Code/map/denmark-latest.osm");
 		Osm rootNode = xmlMapper.deserialize(file);
 		System.out.println("# of Nodes: " + rootNode.getNodes().size());
 	}
--- src/main/java/com/noticemedan/map/data/osm/Osm.java
+++ src/main/java/com/noticemedan/map/data/osm/Osm.java
@@ -5,6 +5,7 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -13,14 +14,14 @@ public class Osm {
 	private @Element Bounds bounds;
 
 	@ElementList(inline = true, entry = "node", required = false)
-	private	List<Node> nodes;
+	private	List<Node> nodes = new ArrayList<>();
 
 	@ElementList(inline = true, entry = "way", required = false)
-	private List<Way> ways;
+	private List<Way> ways = new ArrayList<>();
 
 	@ElementList(inline = true, entry = "tag", required = false)
-	private List<Tag> tags;
+	private List<Tag> tags = new ArrayList<>();
 
 	@ElementList(inline = true, entry = "relation", required = false)
-	private List<Relation> relations;
+	private List<Relation> relations = new ArrayList<>();
 }
