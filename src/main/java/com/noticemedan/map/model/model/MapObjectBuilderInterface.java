efe16d292687da94cedaeef98f308dd68bf24e11
Silind <simonlhoiberg@gmail.com>
2018-03-25 11:56:01 +0200

    Cleaned up a bit

--- src/main/java/com/noticemedan/map/model/model/MapObjectBuilderInterface.java
+++
@@ -1,8 +0,0 @@
-package com.noticemedan.map.model.model;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MapObjectBuilderInterface {
-    Map<OSMType, List<MapObject>> getMapObjectsByType();
-}
