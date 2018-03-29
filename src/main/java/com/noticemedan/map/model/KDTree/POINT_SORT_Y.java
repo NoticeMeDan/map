3b9610e06a507b4bae2b4d22e54e1d45a506eb3b
emilbartholdy <emil.bartholdy@gmail.com>
2018-03-29 11:15:48 +0200

    Add comparator class for sorting KDTree points with regards to their y-value

--- src/main/java/com/noticemedan/map/model/KDTree/POINT_SORT_Y.java
+++ src/main/java/com/noticemedan/map/model/KDTree/POINT_SORT_Y.java
@@ -2,11 +2,11 @@ package com.noticemedan.map.model.KDTree;
 
 import java.util.Comparator;
 
-public class POINT_SORT_X implements Comparator<KDTreePoint> {
+public class POINT_SORT_Y implements Comparator<KDTreePoint> {
 	@Override
 	public int compare(KDTreePoint a, KDTreePoint b) {
-		if (a.getX() > b.getX()) return 1;
-		if (a.getX() < b.getX()) return -1;
+		if (a.getY() > b.getY()) return 1;
+		if (a.getY() < b.getY()) return -1;
 		return 0;
 	}
 }
