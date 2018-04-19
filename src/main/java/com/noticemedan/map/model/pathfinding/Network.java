1a3643e9fd1aefd402585113068138a2672f6576
Silind <simonlhoiberg@gmail.com>
2018-04-19 15:06:55 +0200

    Dijkstra algorithm implemented. Tests passed using test-network

--- src/main/java/com/noticemedan/map/model/pathfinding/Network.java
+++ src/main/java/com/noticemedan/map/model/pathfinding/Network.java
@@ -1,10 +1,37 @@
 package com.noticemedan.map.model.pathfinding;
 
-public class Network {
-	PathNode[] allNodes;
+import com.noticemedan.map.model.utilities.Coordinate;
+import io.vavr.collection.List;
+import lombok.Data;
 
-	public void populateGraph() {
-		// Some functionality that will create and populate a network
-		// @Simon
+@Data
+public class Network {
+	private List<PathNode> allNodes;
+	private List<PathEdge> allEdges;
+
+	public Network() {
+		this.allNodes = List.empty();
+		this.allEdges = List.empty();
 	}
+
+	public void addPathNodes(List<Coordinate> coordinateList) {
+		int id = 0;
+		for (Coordinate coordinate : coordinateList) {
+			this.allNodes = this.allNodes.append(
+					PathNode.builder()
+							.id(id++)
+							.lat(coordinate.getY())
+							.lon(coordinate.getX())
+							.edges(List.empty())
+							.build()
+			);
+		}
+	}
+
+	public void addEdge(PathNode v, PathNode w) {
+		PathEdge pathEdge = new PathEdge(v, w);
+		v.setEdges(v.getEdges().append(pathEdge));
+		allEdges = allEdges.append(pathEdge);
+	}
+
 }
--- src/main/java/com/noticemedan/map/model/pathfinding/PathNode.java
+++ src/main/java/com/noticemedan/map/model/pathfinding/PathNode.java
@@ -1,9 +1,18 @@
 package com.noticemedan.map.model.pathfinding;
 
 import io.vavr.collection.List;
+import lombok.Builder;
+import lombok.Data;
 
+@Builder
+@Data
 public class PathNode {
-	List<PathEdge> edges;
+	private int id;
 	private double lon;
 	private double lat;
+	List<PathEdge> edges;
+
+	public String toString() {
+		return "[id: " + id + " :: {" + lon + ", " + lat + "}]";
+	}
 }
