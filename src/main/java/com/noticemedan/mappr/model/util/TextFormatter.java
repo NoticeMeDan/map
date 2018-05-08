4b7745483d00c58d76f016d94b9c6270340da469
emilbartholdy <emil.bartholdy@gmail.com>
2018-05-08 11:10:33 +0200

    Add test class for TextFormatter

---
+++ src/main/java/com/noticemedan/mappr/model/util/TextFormatter.java
@@ -0,0 +1,13 @@
+package com.noticemedan.mappr.model.util;
+
+public class TextFormatter {
+	static public String formatDistance(Double distance, int decimals) {
+		if(decimals < 0) new RuntimeException("Number of decimals cannot be 0");
+		String distanceAbbreviation = "km";
+		if (distance < 1) distanceAbbreviation = "m";
+		if (distance < 1) distance *= 1000;
+		String format = "%." + decimals + "f";
+		String withoutAbbreviation = String.format(format, distance);
+		return withoutAbbreviation + " " + distanceAbbreviation;
+	}
+}
