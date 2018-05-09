ffcc5d140ffd2eecf7b9bed48ea0d35c14656772
Elias Jørgensen <eliaszakarias@gmail.com>
2018-05-09 21:19:47 +0200

    Use buffered IO streams

--- src/main/java/com/noticemedan/mappr/dao/MapDao.java
+++ src/main/java/com/noticemedan/mappr/dao/MapDao.java
@@ -10,7 +10,7 @@ import java.nio.file.StandardOpenOption;
 public class MapDao implements DataReader<MapData>, DataWriter<MapData> {
 	@Override
 	public MapData read(Path input) throws IOException {
-		try (ObjectInputStream stream = new ObjectInputStream(Files.newInputStream(input, StandardOpenOption.READ))) {
+		try (ObjectInputStream stream = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(input, StandardOpenOption.READ)))) {
 			return MapData.class.cast(stream.readObject());
 		} catch (ClassNotFoundException e) {
 			throw new IOException("An error occurred while reading from .map file.", e);
@@ -19,7 +19,7 @@ public class MapDao implements DataReader<MapData>, DataWriter<MapData> {
 
 	@Override
 	public MapData write(Path output, MapData data) throws IOException {
-		try (ObjectOutputStream stream = new ObjectOutputStream(Files.newOutputStream(output, StandardOpenOption.CREATE_NEW))) {
+		try (ObjectOutputStream stream = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(output, StandardOpenOption.CREATE_NEW)))) {
 			stream.writeObject(data);
 			return data;
 		}
