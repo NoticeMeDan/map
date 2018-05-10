bbf359371184ade1c86e505beab67dc9c85129cb
Elias Jørgensen <eliaszakarias@gmail.com>
2018-05-10 16:56:36 +0200

    Revert magnus' bommert

--- src/main/java/com/noticemedan/mappr/App.java
+++ src/main/java/com/noticemedan/mappr/App.java
@@ -5,11 +5,10 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import com.noticemedan.mappr.model.DomainFacade;
+import com.noticemedan.mappr.viewmodel.MainViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -22,15 +21,10 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
 		loader.setControllerFactory(injector::getInstance);
-		Parent root = loader.load();
-		Image icon = new Image(App.class.getResourceAsStream("/media/icon.png"));
-
-		primaryStage.setTitle("Mappr");
-		primaryStage.getIcons().add(icon);
-		primaryStage.setWidth(1100);
-		primaryStage.setHeight(650);
-		primaryStage.setScene(new Scene(root));
-		primaryStage.show();
+		AnchorPane mainView = loader.load();
+		MainViewController mainViewController = loader.getController();
+		mainViewController.setStage(primaryStage);
+		mainViewController.configureStage();
     }
 
     // Use Dependency Injection to inject DomainFacade into FX Controllers
